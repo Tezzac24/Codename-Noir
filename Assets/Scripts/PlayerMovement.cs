2e7bf1d906e7af8365f28eeb7dae01c4af4bd64f
Tezzac24 <83923814+Tezzac24@users.noreply.github.com>
2023-03-11 03:50:35 +0000

    Startup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     // what you come here to look at u aint got no buzniuss being here lil boah imma show u a mans world kittenr   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
