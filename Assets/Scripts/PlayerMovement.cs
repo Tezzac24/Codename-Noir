d0eca78ed2812df285f8ee19325195be764fe6b3
Tezzac24 <83923814+Tezzac24@users.noreply.github.com>
2023-03-12 00:15:57 +0000

    basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    private float moveSpeed = 5f;
+    private Vector2 movement;
+
     void Start()
     {
-     // what you come here to look at u aint got no buzniuss being here lil boah imma show u a mans world kittenr   
+        // what you come here to look at u aint got no buzniuss being here lil boah imma show u a mans world kittenr   
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
