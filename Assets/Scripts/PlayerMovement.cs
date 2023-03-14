6bb036c699d3208363ec27f044990e88ff24bf89
Tezzac24 <83923814+Tezzac24@users.noreply.github.com>
2023-03-14 03:11:39 +0000

    Dashing mechanic
    Use Left Shift to dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float moveSpeed = 5f;
+    private float activeMoveSpeed;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 2.5f;
 
     private Rigidbody2D rb;
     public Camera cam;
@@ -14,25 +21,56 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         // what you come here to look at u aint got no buzniuss being here lil boah imma show u a mans world kittenr   
+        // Initialises variable 'rb' as Rigidbody2D component
         rb = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = moveSpeed;
     }
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         movement.y = Input.GetAxisRaw("Vertical");
         movement.x = Input.GetAxisRaw("Horizontal");
 
+        // Changes mouseposition val from screen pixels to in World coordinates
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        //Debug.Log(mousePos);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (isDashing)
+        {
+            return;
+        }
+
+        //rb.velocity = movement * activeMoveSpeed;
+        rb.MovePosition(rb.position + movement * activeMoveSpeed * Time.fixedDeltaTime);
         
+        // gets look direction
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
         rb.rotation = angle;
+    }
+
+    private IEnumerator Dash() 
+    {
+        canDash = false;
+        isDashing = true;
+        //rb.gravityScale = 0;
+        rb.velocity = movement * dashingPower;
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
     }
 }
